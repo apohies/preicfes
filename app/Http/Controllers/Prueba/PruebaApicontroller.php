45d110671d01c383a67d0857084e86136b3fa6c1
apohies <miguelegion@gmail.com>
2020-05-01 20:07:31 -0500

    inicio proyecto

---
+++ app/Http/Controllers/Prueba/PruebaApicontroller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Prueba;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\User;
+
+class PruebaApicontroller extends Controller
+{
+    public function index()
+    {
+        $usuarios=User::all();
+
+        return response()->json(['user'=>$usuarios]);
+
+    }
+}
